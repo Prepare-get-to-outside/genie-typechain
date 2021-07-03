801717e843b8c0f44b2f7b824fcca3cddc402235
JinhuiKim <public.jinhui.kim@gmail.com>
2021-07-03 12:51:33 +0900

    Format Document

--- src/index.ts
+++ src/index.ts
@@ -37,27 +37,27 @@ class Block {
     }
 }
 
-const genesisBlock:Block = new Block(0, "20202020", "", "Hello", 123456);
+const genesisBlock: Block = new Block(0, "20202020", "", "Hello", 123456);
 
 let blockchain: Block[] = [genesisBlock];
 
-const getBlockchain = () : Block[] => blockchain;
+const getBlockchain = (): Block[] => blockchain;
 
-const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
-const createNewBlock = (data:string) : Block => {
-    const previousBlock : Block = getLatestBlock();
-    const newIndex : number = previousBlock.index + 1;
-    const newTimestamp : number = getNewTimeStamp();
-    const newHash : string = Block.calculateBlockHash(
+const createNewBlock = (data: string): Block => {
+    const previousBlock: Block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimestamp: number = getNewTimeStamp();
+    const newHash: string = Block.calculateBlockHash(
         newIndex,
         previousBlock.hash,
         newTimestamp,
         data
     );
-    const newBlock : Block = new Block(
+    const newBlock: Block = new Block(
         newIndex,
         newHash,
         previousBlock.hash,
@@ -98,4 +98,4 @@ const addBlock = (candidateBlock: Block): void => {
     }
 }
 
-export {};
+export { };
